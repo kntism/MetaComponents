810d2d14d97b4263b075db08fa5f33647e64f646
Hnss <loozsky@gmail.com>
2022-06-01 17:53:12 +0800

    update

---
+++ src/components/Header/Header.js
@@ -0,0 +1,6 @@
+function Header(props) {
+    const element = <h1>Meta Components</h1>;
+    return element;
+}
+
+export default Header;
---
+++ src/components/Hello.js
@@ -0,0 +1,6 @@
+function Hello(props) {
+    const element = <div>Hello</div>;
+    return element;
+}
+
+export default Hello;
